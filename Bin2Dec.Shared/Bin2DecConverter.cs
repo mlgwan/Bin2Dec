fde1f9764b4bb659dc0cbd756f3cae6db612e999
Ciwan Clifford <18048714+mlgwan@users.noreply.github.com>
2025-11-19 18:30:22 +0100

    Add project files.

---
+++ Bin2Dec.Shared/Bin2DecConverter.cs
@@ -0,0 +1,49 @@
+﻿using System.Numerics;
+
+namespace Bin2Dec.Shared
+{
+    public static class Bin2DecConverter
+    {
+        public static bool InputIsValidBinaryNumber(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return false;
+            }
+            foreach (var symbol in input)
+            {
+                if (symbol != '0' && symbol != '1')
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public static BigInteger ConvertBinaryToDecimal(string binaryNumber)
+        {
+            if (InputIsValidBinaryNumber(binaryNumber))
+            {
+                BigInteger result = 0;
+                for (int i = 0; i < binaryNumber.Length; i++)
+                {
+                    if (binaryNumber[i] == '1')
+                    {
+                        result++;
+                    }
+                    if (i < binaryNumber.Length - 1)
+                    {
+                        result *= 2;
+                    }
+                }
+
+                return result;
+            }
+            else
+            {
+                throw new InvalidDataException();
+            }
+           
+        }
+    }
+}
---
+++ Bin2Dec/Program.cs
@@ -0,0 +1,33 @@
+﻿using Bin2Dec.Shared;
+
+bool userWantsToExit = false;
+
+while (!userWantsToExit)
+{
+    string binaryNumber = "";
+    Console.WriteLine("Please enter a binary number");
+    bool inputIsValid = false;
+    while (!inputIsValid)
+    {
+        var input = Console.ReadLine();
+        if (!String.IsNullOrEmpty(input) && (input.ToLower() == "exit" || input.ToLower() == "quit" || input.ToLower() == "stop"))
+        {
+            userWantsToExit= true;
+            break;
+        }
+        inputIsValid = Bin2DecConverter.InputIsValidBinaryNumber(input);
+        if (!inputIsValid)
+        {
+            Console.WriteLine("Please enter a valid binary number. (Example: 101010)");
+        }
+        else
+        {
+            binaryNumber = input;
+        }
+    }
+    if (!userWantsToExit)
+    {
+        Console.WriteLine($"{binaryNumber} in decimal would be: {Bin2DecConverter.ConvertBinaryToDecimal(binaryNumber)}");
+    }
+
+}
